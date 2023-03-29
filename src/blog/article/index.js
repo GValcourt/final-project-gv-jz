1f806e2ccc873ff58aedf0444c8cd8ba054412df
GValcourt <83992659+GValcourt@users.noreply.github.com>
2023-03-28 21:00:57 -0400

    made basic design for article

--- src/blog/article/index.js
+++ src/blog/article/index.js
@@ -4,8 +4,24 @@
 
 function ArticleComponent(){
     return (
-        <div>
-            ArticleComponent
+        <div className="container">
+            <h1>
+                My Galavant
+            </h1>
+            <h2>
+                By Greg Valcourt
+            </h2>
+            <p>
+                text1
+            </p>
+            <h6>image1</h6>
+            <p>
+                text2
+            </p>
+            <h6>image2</h6>
+            <p>
+                text3
+            </p>
       </div>
       );
 }
