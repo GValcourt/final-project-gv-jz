794c186a3a1ee192164a042470750888cc614790
jzvele <jzvelebilova@gmail.com>
2023-03-31 21:56:28 -0400

    Home component

---
+++ src/blog/home-page/blog-post-grid.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function PostGrid() {
+
+    return (
+        <div className="row">
+            <div className="col-md-4">
+                <div className="card">
+                    <img className="card-img-top" src="./images/gorges-grant-hotel.jpeg" alt="Gorges Grant Hotel"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Blog Post 1</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <a href="#" className="btn btn-primary">Read More</a>
+                        </div>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div className="card">
+                    <img className="card-img-top" src="./images/hotel-vt-lobby.jpeg" alt="Hotel Vermont"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Blog Post 2</h5>
+                            <p className="card-text">Sed do eiusmod tempor incididunt ut labore et dolore magna
+                                aliqua.</p>
+                            <a href="#" className="btn btn-primary">Read More</a>
+                        </div>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div className="card">
+                    <img className="card-img-top" src="./public/images/portland-harbor-hotel.jpeg" alt="Portland Harbor Hotel"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Blog Post 3</h5>
+                            <p className="card-text">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat.</p>
+                            <a href="#" className="btn btn-primary">Read More</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default PostGrid;
--- src/blog/home-page/index.js
+++ src/blog/home-page/index.js
@@ -1,11 +1,50 @@
+import React from "react";
+// import {Routes, Route} from "react-router";
+// import { configureStore } from '@reduxjs/toolkit';
+// import {Provider} from "react-redux";
+import PostGrid from "./blog-post-grid";
+
 
 //renders pretty stuff
 
 function HomeComponent(){
     return (
-        <div>
-            HomeComponent
-      </div>
+        // <Provider store={store}>
+            <div>
+                <div className="row">
+                    {/* Nav and search bar */}
+                </div>
+                <div className="container">
+                    <div className="jumbotron">
+                        <h1>Book It</h1>
+                        <p>Hotel reviews for the modern nomad</p>
+                        <a className="btn btn-primary btn-lg" href="#" role="button">Read More</a>
+                    </div>
+                    <div className="row">
+                        <PostGrid/>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <h3>Top Rated Hotels</h3>
+                            <ul className="list-group">
+                                <li className="list-group-item"><a href="#">Camden Inn</a></li>
+                                <li className="list-group-item"><a href="#">The Federal</a></li>
+                                <li className="list-group-item"><a href="#">The Portland Regency</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-sm-4">
+                            <h3>Follow Me</h3>
+                            <ul className="list-group">
+                                <li className="list-group-item"><a href="#">Facebook</a></li>
+                                <li className="list-group-item"><a href="#">Twitter</a></li>
+                                <li className="list-group-item"><a href="#">Instagram</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        // </Provider>
       );
 }
 
