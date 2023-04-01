9e1da821d8a6b699b77bd6de852e88039e7fbf57
GValcourt <gregory.valcourt@gmail.com>
2023-03-31 20:32:08 -0400

    created CreateArticleComponent
    Still hot garbage

--- src/blog/create-article/index.js
+++ src/blog/create-article/index.js
@@ -1,8 +1,20 @@
-const CreateArticleComponent = ()=>{
+import MapContainer from "../google-map"
+import { Loader } from "@googlemaps/js-api-loader";
 
+
+const CreateArticleComponent = ()=>{
     return (
         <div className="container">
             createcomponent
+            <form>
+            <input defaultValue={"title of article"}>
+            </input><br></br>
+            <input defaultValue={"articletext"}>
+            </input><br></br>
+            image uploads<br></br>
+            <input id="locationstring" defaultValue={"title of article"}>
+            </input>
+            </form>
       </div>
       );
 }
