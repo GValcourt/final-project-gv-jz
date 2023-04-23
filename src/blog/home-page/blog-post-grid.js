17a59dec632146d8673f5f389e861c2e7df37c34
GValcourt <gregory.valcourt@gmail.com>
2023-04-23 12:49:35 -0400

    Front page now accepts article images

--- src/blog/home-page/blog-post-grid.js
+++ src/blog/home-page/blog-post-grid.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { getImageThunk } from "../../services/image-thunk";
+import { useDispatch } from "react-redux";
 
 function PostGrid( articles ) {
     //console.log(articles.articles.articles)
-
+    const dispatch = useDispatch()
+    const [images, setImage] = useState(['../../images/gorges-grant-hotel.jpeg', '../../images/gorges-grant-hotel.jpeg', '../../images/gorges-grant-hotel.jpeg'])
     let firstArticles = articles.articles.articles.slice(0,3)
+    useEffect(() => {
+        for (let i = 0; i < firstArticles.length; i++) {
+            if(firstArticles[i].images[0] !== undefined && firstArticles[i].images[0] !== null
+                && firstArticles[i].images[0] !== ''){
+                //console.log(firstArticles[i].images[0])
+                dispatch(getImageThunk(firstArticles[i].images[0])).then(res => {images.splice(i,1,res.payload)})
+            }
+        }},[articles])
 
-    const gridItem = (article) => {
+    const gridItem = (article, index) => {
+    
 
         return(
             <div className="col-md-4">
                 <div className="card">
-                    <img className="card-img-top" src="./images/gorges-grant-hotel.jpeg" alt="Gorges Grant Hotel"/>
+                    <img className="card-img-top" src={images[index]} alt="Gorges Grant Hotel"/>
                         <div className="card-body">
                             <h5 className="card-title">Blog Post</h5>
                             <p className="card-text">{article.title}</p>
@@ -24,7 +35,7 @@ function PostGrid( articles ) {
 
     return (
         <div className="row">
-            {firstArticles.map(gridItem)}
+            {firstArticles.map((currentElement, index) => gridItem(currentElement, index))}
         </div>
     )
 
