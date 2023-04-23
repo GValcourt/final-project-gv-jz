d825e0a40d8f861692c2a5d2df19cb738339ba92
GValcourt <gregory.valcourt@gmail.com>
2023-04-23 19:00:54 -0400

    Added stylesheet to src, removed bootswatch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import Blog from "./blog"
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
-import CurrentUserContext from "./blog/current-user-context";
-import ProfileComponent from "./blog/profile/index";
-import LoginComponent from "./blog/login-page/index";
-import NavigationBar from "./blog/navigation-bar/index";
-import AdminComponent from "./blog/admin/index";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import articleReducer from "./blog/reducers/article-reducer";
-import authReducer from "./blog/reducers/auth-reducer";
+import './stylesheet/index.css'
+
 
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './vendors/bootswatch/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
