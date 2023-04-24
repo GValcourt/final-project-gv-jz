0dfa1f584ea466ddb9b730f5edd5de1fe80e8b52
jzvele <jzvelebilova@gmail.com>
2023-04-23 22:18:16 -0400

    Fixed admin page

--- src/blog/admin/index.js
+++ src/blog/admin/index.js
@@ -5,27 +5,33 @@ import { Card, Button } from 'react-bootstrap';
 
 const AdminComponent = () => {
     const currentUser = useSelector(state => state.auth.currentUser);
-    const isAdmin = currentUser && currentUser.type === 'sysadmin';
+    const isAdmin = currentUser && currentUser.user_type === 'sysadmin';
     const navigate = useNavigate();
     if (!isAdmin) {
-        navigate("/home");
-    }
+        return(
+            <div>
+                <p>
+                    You must be an admin to view this page.
+                </p>
+            </div>
+        )
+     }
 
     return (
-        <div>
-            <h2>Admin Page</h2>
+        <div className="container m-3">
+            <h2 className="display-4 font-24">Admin Page</h2>
             <Card className="mb-3">
                 <Card.Body>
                     <Card.Title>User Management</Card.Title>
                     <Card.Text>View, add, edit, or delete user accounts.</Card.Text>
-                    <Button variant="primary">Go to User Management</Button>
+                    <Button variant="secondary">Go to User Management</Button>
                 </Card.Body>
             </Card>
             <Card className="mb-3">
                 <Card.Body>
                     <Card.Title>System Settings</Card.Title>
                     <Card.Text>View or modify system settings.</Card.Text>
-                    <Button variant="primary">Go to System Settings</Button>
+                    <Button variant="secondary">Go to System Settings</Button>
                 </Card.Body>
             </Card>
         </div>
